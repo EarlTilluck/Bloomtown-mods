93b9d2a5c5e3636c635fabfeb7e6f20a88e2e199
Earl Tilluck <37227663+EarlTilluck@users.noreply.github.com>
2025-07-14 02:04:59 -0400

    added cheats

---
+++ bloomtown_cheats/PatchStackable.cs
@@ -0,0 +1,184 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Uroboros.Extension;
+
+namespace bloomtown_cheats
+{
+    [Harmony]
+    class PatchStackable
+    {
+
+        public static List<string> consumables = new List<string>
+        {
+            "Apple",
+            "AppleJelly",
+            "Banana",
+            "BananaChips",
+            "CentralFish1_Cooked",
+            "CentralFish2_Cooked",
+            "CentralFish3_Cooked",
+            "CentralFish4_Cooked",
+            "Cheeseburger",
+            "Chips",
+            "ChocolateBar",
+            "Coffee",
+            "CommonFish1_Cooked",
+            "CommonFish2_Cooked",
+            "CommonFish3_Cooked",
+            "CraftMaterial_butter",
+            "Deer's Pleasure",
+            "Demon_Catcher",
+            "Doughnut",
+            "Down_bomb",
+            "Electric_bomb",
+            "FarmFish1_Cooked",
+            "FarmFish2_Cooked",
+            "FarmFish3_Cooked",
+            "FarmFish4_Cooked",
+            "Fire_bomb",
+            "ForestFish1_Cooked",
+            "ForestFish2_Cooked",
+            "ForestFish3_Cooked",
+            "ForestFish4_Cooked",
+            "Free_Candy",
+            "Freeze_bomb",
+            "Fried Bread",
+            "FriedChicken",
+            "FriedEggs",
+            "GenericLollipop",
+            "GummyBear",
+            "Heal_all",
+            "HotDog",
+            "IceCream",
+            "LiocoriceCandy",
+            "MintCandy",
+            "Orange",
+            "OrangeJuice",
+            "Revival Bead",
+            "Revival_Basic",
+            "Revive_All",
+            "Smoke_screen",
+            "Soda",
+            "Status_healer",
+            "Wooze_bomb",
+            "Yakisoba Pan"
+        };
+
+        public static List<string> materials = new List<string>
+        {
+            "Bait",
+            "CraftMaterial_cabbage",
+            "CraftMaterial_chicken",
+            "DaisyFlower",
+            "demonic_cow_milk",
+            "demonic_dressing",
+            "CraftMaterial_eggs",
+            "CraftMaterial_ketchup",
+            "laxative",
+            "Licorice",
+            "Material_Tier_1",
+            "Material_Tier_2",
+            "Material_Tier_3",
+            "Mint",
+            "purple_tentacles",
+            "Sugar",
+            "SweetCorn",
+            "CraftMaterial_tomatoes",
+            "CraftMaterial_wheat",
+        };
+
+        public static List<string> fishes = new List<string>
+        {
+            "CentralFish1",
+            "CentralFish2",
+            "CentralFish3",
+            "CentralFish4",
+            "CommonFish1",
+            "CommonFish2",
+            "CommonFish3",
+            "FarmFish1",
+            "FarmFish2",
+            "FarmFish3",
+            "FarmFish4",
+            "ForestFish1",
+            "ForestFish2",
+            "ForestFish3",
+            "ForestFish4"
+        };
+
+        public const string lockpick = "Lockpick";
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Stackable), nameof(Stackable.Take))]
+        public static void Stackable_Take(Stackable __instance, ref int amount)
+        {
+
+            ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("Logger");
+            string id = __instance.uid;
+
+            // if cheat enbabled...
+            if (Plugin.EnableConsumableStay.Value == true)
+            {
+                if (consumables.Contains(id))
+                {
+                    __instance.Add(amount);
+                }
+            }
+            if (Plugin.EnableMaterialStay.Value == true)
+            {
+                if (materials.Contains(id))
+                {
+                    __instance.Add(amount);
+                }
+            }
+            if (Plugin.EnableFishStay.Value == true)
+            {
+                if (fishes.Contains(id))
+                {
+                    __instance.Add(amount);
+                }
+            }
+            if (Plugin.EnableLockpickStay.Value == true)
+            {
+                if (id == lockpick)
+                {
+                    __instance.Add(amount);
+                }
+            }
+
+
+
+            //foo
+
+            //GameSettings gameSettings = StatsSync.instance.gameSettings;
+            ////List<Consumable> allObjects = Extensions.EditorMethodsWrapper.LoadAllFromMovedResources<Consumable>(Consumable.PathToResources).ToList<Consumable>();
+            //List<CraftMaterial> list = gameSettings.craftMaterials;
+
+            //logger.LogError("item count is: " + list.Count);
+            //foreach (CraftMaterial consumable in list)
+            //{
+            //    string u = consumable.uid;
+            //    logger.LogInfo("item : " + u);
+            //}
+
+            //var lockpick = gameSettings.lockpick;
+            //logger.LogInfo(lockpick.uid);
+
+            //List<Fish> fishes = gameSettings.fishes;
+            //foreach (Fish fish in fishes)
+            //{
+            //    logger.LogInfo(fish.uid);
+            //}
+
+            // foo
+
+
+        }
+    }
+}
---
+++ bloomtown_cheats/Plugin.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uroboros.Extension;
+
+namespace bloomtown_cheats
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // description of this mod
+        private const string modGUID = "codesprint.bloomtown_cheats";
+        private const string modName = "Bloomtown Cheats";
+        private const string modVersion = "1.0.0";
+
+        // we need harmony to patch our code into the game
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        // BepInEx configurable values 
+        public static ConfigEntry<bool> EnableConsumableStay;
+        public static ConfigEntry<bool> EnableMaterialStay;
+        public static ConfigEntry<bool> EnableFishStay;
+        public static ConfigEntry<bool> EnableLockpickStay;
+
+        void Awake()
+        {
+
+            // init BepInEx configuration value
+            EnableConsumableStay = Config.Bind(
+                "General",              // Config section
+                "Infinte consumable Use.",         // Config key
+                true,                     // Default value
+                "Food, meds and battle items do not decrease when used."       // Description
+            );
+
+            EnableMaterialStay = Config.Bind(
+                "General",              // Config section
+                "Infinte material use.",         // Config key
+                true,                     // Default value
+                "Materials used in crafting do not decrease when used."       // Description
+            );
+
+            EnableFishStay = Config.Bind(
+                "General",              // Config section
+                "Infinte raw fish use.",         // Config key
+                true,                     // Default value
+                "Raw fishes do not decrease when cooked or used."       // Description
+            );
+
+            EnableLockpickStay = Config.Bind(
+               "General",              // Config section
+               "Infinte Lockpicks use.",         // Config key
+               true,                     // Default value
+               "Lockpick amount does not decrease when used."       // Description
+           );
+
+            // on init, run patch all to patch our code into the game 
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(PatchStackable));
+
+            Logger.LogInfo("Bloomtown Cheats Loaded.");
+
+
+        }
+
+    }
+}
